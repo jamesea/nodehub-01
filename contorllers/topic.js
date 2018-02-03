869a643663af6036cf54e61a9d8bbc4bd5889d66
jamesea <398165908@qq.com>
2018-02-03 20:38:21 +0800

    提取划分控制器

---
+++ contorllers/topic.js
@@ -0,0 +1,23 @@
+exports.showCreate = (req,res) =>{
+	res.send('get showCreate')
+}
+
+exports.create = (req,res) =>{
+	res.send('get create')
+}
+
+exports.showDetail = (req,res) =>{
+	res.send('get showDetail')
+}
+
+exports.showEdit = (req,res) =>{
+	res.send('get showEdit')
+}
+
+exports.edit = (req,res) =>{
+	res.send('get edit')
+}
+
+exports.delete = (req,res) =>{
+	res.send('get delete')
+}
---
+++ contorllers/user.js
@@ -0,0 +1,15 @@
+exports.showSignin = (req,res) => {
+	res.send('get showSignin')
+}
+exports.signin = (req,res) => {
+	res.send('get signin')
+}
+exports.showSignup = (req,res) => {
+	res.send('get showSignup')
+}
+exports.signup = (req,res) => {
+	res.send('get signup')
+}
+exports.signout = (req,res) => {
+	res.send('get signout')
+}
