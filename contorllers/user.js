ed5918415e9c52374fe9d31ad7417b3c3c54af83
jamesea <398165908@qq.com>
2018-02-04 12:00:26 +0800

    处理注册页面表单提交

--- contorllers/user.js
+++ contorllers/user.js
@@ -2,13 +2,17 @@ exports.showSignin = (req,res) => {
 	res.render('signin.html')
 }
 exports.signin = (req,res) => {
-	res.send('get signin')
+	res.send(JOSN.stringify({
+		foo:'bra'
+	}))
 }
 exports.showSignup = (req,res) => {
 	res.render('signup.html')
 }
 exports.signup = (req,res) => {
-	res.send('get signup')
+	res.json({
+		post:'asdni'
+	})
 }
 exports.signout = (req,res) => {
 	res.send('get signout')
